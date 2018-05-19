3d552a836dc260ff4e6505de90c822dac41c3c36
sipwarriper <sipwarriper@gmail.com>
2018-05-19 14:52:03 +0200

    Importació highlights.js i exemple de codi

---
+++ CustomScripts.js
@@ -0,0 +1,16 @@
+$(function() {
+    $("#menu").load("menu.html");
+
+    function activeNav() {
+        var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
+         $("#menu ul li a").each(function(){
+              if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+              $(this).addClass("active");
+         });
+    }
+
+    setTimeout(function() {
+        activeNav();
+    }, 100);
+
+});
