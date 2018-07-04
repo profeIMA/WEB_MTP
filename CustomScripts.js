df2ff9c8948add73bf9c921643c5c4c7da9280f5
Onnion19 <aleixrius19@gmail.com>
2018-07-04 10:53:42 +0200

    IMPORTAT! CANVI DE PATHS
    CustomScript, s'ha canviat el path del menu. (/menu.html) a (../../menu.html) //// S'ha canviat el path del hljs (s'ha afegit https://....)///// S'ha canviat el path del CustomScript a: (../../CustomScript.js)

--- CustomScripts.js
+++ CustomScripts.js
@@ -1,7 +1,7 @@
 
 
 $(function() {
-    $("#menu").load("/menu.html");
+    $("#menu").load("../../menu.html");
 
     function activeNav() {
         var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
